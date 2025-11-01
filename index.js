90528e4e26de4408a9fe32ca5c5894b16a8850a2
Katina001 <Katina82@hotmail.com>
2025-11-01 10:37:49 -0600

    First portfolio upload

---
+++ index.js
@@ -0,0 +1,7 @@
+// const greeting = document.getElementById("greeting")
+// greeting.style.textAlign = "center"
+// greeting.style.marginTop = "50px"
+// const username = window.prompt ("what's your name?","Miles and Tessa!")
+// greeting.innerHTML = "Welcome to our website, " + username + ""
+
+
